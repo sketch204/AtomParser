4f96549c141d99612e0eb509fa46698cd55908d8
Inal Gotov <sketch204@gmail.com>
2023-11-11 20:32:43 -0500

    Refactor `Feed` initializers

--- Sources/AtomParser/Feed+Parsing.swift
+++ Sources/AtomParser/Feed+Parsing.swift
@@ -2,21 +2,19 @@ import Foundation
 import AtomXML
 
 extension Feed {
+    private init(parser: AtomXMLParser) throws {
+        try self.init(xmlNode: parser.parse())
+    }
+    
     public init(contentsOf url: URL) throws {
-        let parser = try AtomXMLParser(contentsOf: url)
-        let tree = try parser.parse()
-        try self.init(xmlNode: tree)
+        try self.init(parser: try AtomXMLParser(contentsOf: url))
     }
     
     public init(data: Data) throws {
-        let parser = AtomXMLParser(data: data)
-        let tree = try parser.parse()
-        try self.init(xmlNode: tree)
+        try self.init(parser: AtomXMLParser(data: data))
     }
     
     public init(string: String) throws {
-        let parser = try AtomXMLParser(string: string)
-        let tree = try parser.parse()
-        try self.init(xmlNode: tree)
+        try self.init(parser: try AtomXMLParser(string: string))
     }
 }
